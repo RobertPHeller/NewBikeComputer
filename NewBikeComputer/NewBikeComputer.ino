5bdf59234358f3e36fc7f5cc9c68b511b62e6e46
Robert Heller <heller@deepsoft.com>
2022-06-30 15:33:28 -0400

    Merge local files to github.

---
+++ NewBikeComputer/NewBikeComputer.ino
@@ -0,0 +1,51 @@
+// -!- C++ -!- //////////////////////////////////////////////////////////////
+//
+//  System        : 
+//  Module        : 
+//  Object Name   : $RCSfile$
+//  Revision      : $Revision$
+//  Date          : $Date$
+//  Author        : $Author$
+//  Created By    : Robert Heller
+//  Created       : Thu Jun 30 15:03:26 2022
+//  Last Modified : <220630.1503>
+//
+//  Description	
+//
+//  Notes
+//
+//  History
+//	
+/////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (C) 2022  Robert Heller D/B/A Deepwoods Software
+//			51 Locke Hill Road
+//			Wendell, MA 01379-9728
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// 
+//
+//////////////////////////////////////////////////////////////////////////////
+
+static const char rcsid[] = "@(#) : $Id$";
+
+void setup() {
+    // put your setup code here, to run once:
+}
+                
+void loop() {
+    // put your main code here, to run repeatedly:
+}    
---
+++ README.md
@@ -0,0 +1,5 @@
+# New Bike Computer.
+
+Based on the Adafruit ESP32-S3 TFT Feather, with a Adafruit Ultimate GPS
+FeatherWing.  Mounted in a PacTec FLXT_2535 box.
+
